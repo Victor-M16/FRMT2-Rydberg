1ad1fd59c8ca071683e812793c56dd3190f35d49
Victor Chisomo Mjimapemba <vcmjimapemba@gmail.com>
2023-11-20 16:47:23 +0200

    businesses CRUD

--- users/forms.py
+++ users/forms.py
@@ -5,25 +5,61 @@ from .models import NewUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = NewUser
-        fields = ['email', 'user_name', 'user_type', 'password1', 'password2']
+        fields = ['email', 'user_name', 'user_type', 'is_staff', 'is_superuser', 'password1', 'password2']
 
     user_type = forms.ChoiceField(
         label='User Type',
         widget=forms.Select,
-        choices= NewUser.USER_TYPE_CHOICES, 
+        choices=NewUser.USER_TYPE_CHOICES,
         required=True,
     )
+
+    is_staff = forms.BooleanField(
+        label='Create as Staff',
+        required=False,
+    )
+
+    is_superuser = forms.BooleanField(
+        label='Create as Superuser',
+        required=False,
+    )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.user_type = self.cleaned_data['user_type']
+
+        # Check if the user is to be created as staff
+        user.is_staff = self.cleaned_data.get('is_staff')
+
+        # Check if the user is to be created as a superuser
+        if self.cleaned_data.get('is_superuser'):
+            user.is_staff = True
+            user.is_superuser = True
+
+        if commit:
+            user.save()
+        return user
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = NewUser
-        fields = ['email', 'user_name', 'user_type']
+        fields = ['email', 'user_name', 'user_type', 'is_staff', 'is_superuser']
 
     user_type = forms.ChoiceField(
         label='User Type',
         widget=forms.Select,
         choices=NewUser.USER_TYPE_CHOICES,  
         required=True,
+    )
+
+    is_staff = forms.BooleanField(
+        label='Is Staff',
+        required=False,
+    )
+
+    is_superuser = forms.BooleanField(
+        label='Is Superuser',
+        required=False,
     )
 
 
--- users/urls.py
+++ users/urls.py
@@ -6,24 +6,53 @@ from .views import (
     Collection_instanceCreateView,
     Collection_instanceUpdateView,
     Collection_instanceDeleteView,
+    NewUserListView,
+    NewUserCreateView,
+    NewUserDetailView,
+    NewUserUpdateView,
     )
 from . import views
 
 
 urlpatterns = [
+      path('', views.landing, name='frmt-landing'),
      path('', views.home, name='frmt-home'),
+
      #path('CI/', views.displayCollectionInstances, name='frmt-c-instances'),
-     path('register/', views.register, name='frmt-register'),
-     path('profile/', views.profile, name = 'frmt-profile'),
+     #path('register/', views.register, name='frmt-register'),
+     
      path('dashboard/', views.dashboard, name = 'frmt-dashboard'),
      path('dashboard/parameters/', views.parameters, name = 'frmt-parameters'),
-     path('myadmin/systemusers/', views.displayUsers, name = 'frmt-users'),
-     path('faq/', views.faq, name = 'frmt-faq'),
+
+
+
+     #urls for the admin
+     path('myadmin/systemusers/', NewUserListView.as_view(), name = 'frmt-users'),
+     path('myadmin/register/', NewUserCreateView.as_view(), name='frmt-register'),
+     path('profile/', views.profile, name = 'frmt-profile'),
+     path('sytemusers/<int:pk>/', NewUserDetailView.as_view(), name = 'frmt-newuser-detail'),
+     path('sytemusers/<int:pk>/update/', NewUserUpdateView.as_view(), name = 'frmt-newuser-update'),
+
+
+
+
+     path('properties/', views.displayProperties, name = 'frmt-properties'),
+
+
+
+     #url patterns for collector related collection instance interactions
+     path('mCI/', my_Collection_instanceListView.as_view(), name='frmt-my-c-instances'),
+     
+     
+     #url patterns for council official related collection instance interactions
      path('CI/', Collection_instanceListView.as_view(), name='frmt-c-instances'),
      path('CI/new/', Collection_instanceCreateView.as_view(), name='frmt-CI-create'),
      path('CI/<int:pk>/', Collection_instanceDetailView.as_view(), name='frmt-CI-detail'),
      path('CI/<int:pk>/update/', Collection_instanceUpdateView.as_view(), name='frmt-CI-update'),
      path('CI/<int:pk>/delete/',Collection_instanceDeleteView.as_view(),name='frmt-CI-delete'),
-     path('mCI/', my_Collection_instanceListView.as_view(), name='frmt-my-c-instances'),
-     path('properties/', views.displayProperties, name = 'frmt-properties'),
+     path('view-collectors', views.displayCollectors, name = 'frmt-view-collectors'),
+     
+     
+     #extras
+     path('faq/', views.faq, name = 'frmt-faq'),
 ]
